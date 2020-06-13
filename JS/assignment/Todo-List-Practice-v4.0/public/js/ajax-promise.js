d2cf5dfeb23343ee0586fa8d4efa3be75e30520e
detrox20 <detrox2020@gmail.com>
2020-06-13 22:22:22 +0900

    feat: use promise

---
+++ JS/assignment/Todo-List-Practice-v4.0/public/js/ajax-promise.js
@@ -0,0 +1,59 @@
+// eslint-disable-next-line import/prefer-default-export
+export const ajax = (() => {
+  const req = (method, url, payload) => new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(method, url);
+    xhr.setRequestHeader('content-type', 'application/json');
+    xhr.send(JSON.stringify(payload));
+
+    xhr.onload = () => {
+      if (xhr.status === 200 || xhr.status === 201) {
+        resolve(JSON.parse(xhr.response));
+      } else {
+        reject(new Error(xhr.status));
+      }
+    };
+  });
+
+  return {
+    get(url) {
+      return req('GET', url);
+    },
+    post(url, payload) {
+      return req('POST', url, payload);
+    },
+    patch(url, payload) {
+      return req('PATCH', url, payload);
+    },
+    delete(url) {
+      return req('DELETE', url);
+    }
+  };
+})();
+
+export const request = {
+  get(url) {
+    return fetch(url);
+  },
+
+  post(url, payload) {
+    return fetch(url, {
+      method: 'POST',
+      headers: { 'content-Type': 'application/json' },
+      body: JSON.stringify(payload)
+    });
+  },
+
+  patch(url, payload) {
+    return fetch(url, {
+      method: 'PATCH',
+      headers: { 'content-Type': 'application/json' },
+      body: JSON.stringify(payload)
+    });
+  },
+
+  delete(url) {
+    return fetch(url, { method: 'DELETE' });
+  }
+};
